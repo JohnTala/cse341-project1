44935825ed399e3b64a45405dac935ef154da1c0
JohnTala <57272492+JohnTala@users.noreply.github.com>
2026-03-24 13:33:48 +0200

    Update Swagger host and schemes to point to Render deployment so API docs work live

--- swagger.js
+++ swagger.js
@@ -5,11 +5,17 @@ const doc = {
     title: 'Contacts API',
     description: 'API documentation for the Contacts project'
   },
-  host: 'localhost:8080',
-  schemes: ['http']
+  host: 'cse341-project1-1-4omy.onrender.com', // Render URL
+  schemes: ['https'] // 
 };
 
 const outputFile = './swagger.json';
-const endpointsFiles = ['./routes/contacts.js','./routes/index.js'];
+const endpointsFiles = [
+  './routes/contacts.js',
+  './routes/index.js'
+];
 
-swaggerAutogen(outputFile, endpointsFiles, doc);
+swaggerAutogen(outputFile, endpointsFiles, doc)
+  .then(() => {
+    console.log('Swagger documentation generated successfully!');
+  });

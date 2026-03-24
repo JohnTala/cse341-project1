b34a98d5d19f9d1f723a3901ec2b71202a488869
JohnTala <57272492+JohnTala@users.noreply.github.com>
2026-03-24 12:26:54 +0200

    refactor swagger.js

--- swagger.js
+++ swagger.js
@@ -3,13 +3,23 @@ const swaggerAutogen = require('swagger-autogen')();
 const doc = {
   info: {
     title: 'Contacts API',
-    description: 'API documentation for the Contacts project'
+    description: 'API documentation for the Contacts project',
+    version: '1.0.0'
   },
-  host: 'https://cse341-project1-1-4omy.onrender.com',
-  schemes: ['https']
+  host: process.env.SWAGGER_HOST || '', // Dynamic host
+  basePath: '/',
+  schemes: ['https', 'http'],
+  consumes: ['application/json'],
+  produces: ['application/json']
 };
 
 const outputFile = './swagger.json';
-const endpointsFiles = ['./routes/index.js'];
+const endpointsFiles = [
+  './routes/index.js',
+  './routes/contacts.js' // Include contacts routes
+];
 
-swaggerAutogen(outputFile, endpointsFiles, doc);
+swaggerAutogen(outputFile, endpointsFiles, doc)
+  .then(() => {
+    console.log('Contacts Swagger documentation generated!');
+  });
